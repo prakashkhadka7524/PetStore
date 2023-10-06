955937eed6bc83290d5422a146263cc892188339
prakashkhadka7524 <prakashkhadka7524@outlook.com>
2023-10-06 14:08:05 -0700

    updating the pet store

--- src/main/java/pet/store/controller/model/PetStoreController.java
+++ src/main/java/pet/store/controller/model/PetStoreController.java
@@ -1,10 +1,17 @@
 package pet.store.controller.model;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 import pet.store.entity.PetStore;
 import pet.store.service.PetStoreService;
@@ -26,6 +33,7 @@ public class PetStoreController {
 
     @PutMapping("/update/{petStoreId}")
     public PetStoreData updatePetStore(@PathVariable Long petStoreId, @RequestBody PetStoreData petStoreData) {
+        petStoreData.setPetStoreId(petStoreId);
         log.info("Updating Pet Store", petStoreData);
         return petStoreService.savePetStore(petStoreData);
     }
